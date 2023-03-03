66a272a97f8cf845b40b21d18695ff5903daa6d7
Farouk Hamadi <faroukhamadi1@outlook.com>
2023-03-03 19:23:00 +0100

    add timer file

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,16 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+// Make counter animation by increasing from 0 to 1000
+
+const increaseCounter = () => {
+	const counter = document.getElementById('counter') as HTMLSpanElement;
+	let count = 0;
+
+	const timer = setInterval(() => {
+		count++;
+		counter.innerText = count.toString();
+		if (count === 1593) {
+			clearInterval(timer);
+		}
+	}, 0.5);
+};
+
+export default increaseCounter;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 import '../src/style.css';
+import increaseCounter from './counter';
+
+increaseCounter();
