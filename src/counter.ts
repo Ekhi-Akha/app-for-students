d7828860be37a35fe436ec6dacd062390cce4fc4
Farouk Hamadi <faroukhamadi1@outlook.com>
2023-03-20 19:38:26 +0100

    update counter and cards

--- src/counter.ts
+++ src/counter.ts
@@ -1,16 +1,18 @@
-// Make counter animation by increasing from 0 to 1000
-
 const increaseCounter = () => {
 	const counter = document.getElementById('counter') as HTMLSpanElement;
 	let count = 0;
 
 	const timer = setInterval(() => {
-		count++;
+		if (count < 1550) {
+			count += 10;
+		} else {
+			count++;
+		}
 		counter.innerText = count.toString();
 		if (count === 1593) {
 			clearInterval(timer);
 		}
-	}, 0.5);
+	}, 0.0001);
 };
 
 export default increaseCounter;
