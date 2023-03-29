aa918e5769a70144577fe84836c65c704e4b9ab7
malek <melekbenarbi2@gmail.com>
2023-03-29 16:07:40 +0100

    validation login registration

--- login/login.js
+++ login/login.js
@@ -1,17 +1,45 @@
-$('#signup').click(function () {
-	$('.message').css('transform', 'translateX(100%)');
-	if ($('.message').hasClass('login')) {
-		$('.message').removeClass('login');
-	}
-	$('.message').addClass('signup');
+const loginForm = document.querySelector("#login-form");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  clearErrors();
+
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  // Validate username
+  if (username === "") {
+    showError(usernameInput, "Username is required.");
+  } else if (username.length < 3) {
+    showError(usernameInput, "Username must be at least 3 characters.");
+  }
+
+  // Validate password
+  if (password === "") {
+    showError(passwordInput, "Password is required.");
+  } else if (password.length < 6) {
+    showError(passwordInput, "Password must be at least 6 characters.");
+  } else if (!/\d/.test(password)) {
+    showError(passwordInput, "Password must contain at least one number.");
+  }
+
+  // If there are no errors, submit the form
+  if (document.querySelectorAll(".error-message").length === 0) {
+    loginForm.submit();
+  }
 });
 
-$('#login').click(function () {
-	$('.message').css('transform', 'translateX(0)');
-	if ($('.message').hasClass('login')) {
-		$('.message').removeClass('signup');
-	}
-	$('.message').addClass('login');
-});
+function showError(input, message) {
+  const errorSpan = input.nextElementSibling;
+  errorSpan.textContent = message;
+  input.classList.add("error");
+}
 
-console.log('login.js loaded');
+function clearErrors() {
+  const errorSpans = document.querySelectorAll(".error-message");
+  errorSpans.forEach((error) => (error.textContent = ""));
+  const inputs = document.querySelectorAll(".input-control input");
+  inputs.forEach((input) => input.classList.remove("error"));
+}
