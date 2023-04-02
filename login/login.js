9f4cb800ff3446c44e7a07bcfb98d34e6a397dc0
malek <melekbenarbi2@gmail.com>
2023-04-02 21:10:06 +0100

    login validation

--- login/login.js
+++ login/login.js
@@ -1,56 +1,61 @@
-const loginForm = document.querySelector("#login-form");
-const usernameInput = document.querySelector("#username");
-const passwordInput = document.querySelector("#password");
+const form = document.querySelector('form');
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="password"]');
+const rememberCheckbox = document.querySelector('#remember');
 
-usernameInput.addEventListener("input", (event) => {
-  const username = event.target.value.trim();
-  if (username === "") {
-    showError(usernameInput, "Username is required.");
-    usernameInput.setAttribute("aria-invalid", true);
-  } else if (username.length < 3) {
-    showError(usernameInput, "Username must be at least 3 characters.");
-    usernameInput.setAttribute("aria-invalid", true);
-  } else {
-    clearError(usernameInput);
-    usernameInput.setAttribute("aria-invalid", false);
-  }
-});
-
-passwordInput.addEventListener("input", (event) => {
-  const password = event.target.value.trim();
-  if (password === "") {
-    showError(passwordInput, "Password is required.");
-    passwordInput.setAttribute("aria-invalid", true);
-  } else if (password.length < 6) {
-    showError(passwordInput, "Password must be at least 6 characters.");
-    passwordInput.setAttribute("aria-invalid", true);
-  } else if (!/\d/.test(password)) {
-    showError(passwordInput, "Password must contain at least one number.");
-    passwordInput.setAttribute("aria-invalid", true);
-  } else {
-    clearError(passwordInput);
-    passwordInput.setAttribute("aria-invalid", false);
-  }
+const inputs = document.querySelectorAll('input');
+inputs.forEach((input) => {
+	input.addEventListener('blur', () => {
+		if (input.value.trim() === '') {
+			input.setAttribute('aria-invalid', 'true');
+		}
+	});
 });
 
 function showError(input, message) {
-  const errorSpan = input.nextElementSibling;
-  errorSpan.textContent = message;
-  input.classList.add("error");
+	input.setAttribute('aria-invalid', 'true');
+	const errorContainer = input.parentElement.querySelector('.error-message');
+	if (errorContainer) {
+		errorContainer.textContent = message;
+		errorContainer.style.display = 'block';
+	}
 }
 
-function clearError(input) {
-  const errorSpan = input.nextElementSibling;
-  errorSpan.textContent = "";
-  input.classList.remove("error");
+function showSuccess(input) {
+	input.setAttribute('aria-invalid', 'false');
+	const errorContainer = input.parentElement.querySelector('.error-message');
+	if (errorContainer) {
+		errorContainer.style.display = 'none';
+	}
 }
 
-function clearErrors() {
-  const errorSpans = document.querySelectorAll(".error-message");
-  errorSpans.forEach((error) => (error.textContent = ""));
-  const inputs = document.querySelectorAll(".input-control input");
-  inputs.forEach((input) => {
-    input.classList.remove("error");
-    input.setAttribute("aria-invalid", false);
-  });
+function checkEmail() {
+	const email = emailInput.value.trim();
+	if (email === '') {
+		showError(emailInput, 'Email is required.');
+	} else if (!/\S+@\S+\.\S+/.test(email)) {
+		showError(emailInput, 'Email is not valid.');
+	} else {
+		showSuccess(emailInput);
+	}
 }
+
+function checkPassword() {
+	const password = passwordInput.value.trim();
+	if (password === '') {
+		showError(passwordInput, 'Password is required.');
+	} else if (password.length < 6) {
+		showError(passwordInput, 'Password must be at least 6 characters.');
+	} else {
+		showSuccess(passwordInput);
+	}
+}
+
+emailInput.addEventListener('input', checkEmail);
+passwordInput.addEventListener('input', checkPassword);
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	checkEmail();
+	checkPassword();
+});
